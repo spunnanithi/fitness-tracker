426e7b765e2e2c594000e4c1cbb4945605640161
Sirasit Punnanithi <spunnanithi@gmail.com>
2023-08-22 20:22:55 -0700

    adjusted .env file to include entire mongodb url for security concerns

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,26 +10,22 @@ const app: Express = express();
 app.use(express.json()); // Express middleware function that allows JSON POST requests
 
 const port = process.env.PORT;
-const dbUsername = process.env.DB_USERNAME;
-const dbPassword = process.env.DB_PASSWORD;
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Express + TypeScript Server");
 });
 
 app.post("/workouts", async (req: Request, res: Response) => {
-	console.log(req.body);
-	const newWorkout = new Workout(req.body);
+	const newWorkout = new Workout({
+		name: req.body.name,
+	});
 	const createdWorkout = await newWorkout.save();
 	res.json(createdWorkout);
 });
 
-const db = mongoose
-	.connect(
-		`mongodb+srv://${dbUsername}:${dbPassword}@cluster0.d8kohlr.mongodb.net/?retryWrites=true&w=majority`
-	)
-	.then(() => {
-		app.listen(port, () => {
-			console.log(`[server]: Server is running at http://localhost:${port}`);
-		});
+// Mongoose connection to MongoDB
+mongoose.connect(process.env.MONGODB_URL!).then(() => {
+	app.listen(port, () => {
+		console.log(`[server]: Server is running at http://localhost:${port}`);
 	});
+});
