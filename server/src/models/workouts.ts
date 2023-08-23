01b485b980eabe36f38bb760841004355986737f
Sirasit Punnanithi <spunnanithi@gmail.com>
2023-08-22 20:05:42 -0700

    working on setting up server with mongodb and workouts model

--- server/src/models/workouts.ts
+++ server/src/models/workouts.ts
@@ -1,1 +1,12 @@
-import { Model, Schema, Document, ObjectId } from "mongoose";
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+// const ObjectId = Schema.ObjectId;
+
+const WorkoutSchema = new Schema({
+	name: String,
+});
+
+const WorkoutModel = mongoose.model("Workout", WorkoutSchema);
+
+export default WorkoutModel;
